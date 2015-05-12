beaf148d13ebf3d473f605ad42698d061d641d56
jmdranola <jmdranola@gmail.com>
2015-05-12 13:51:03 +0800

    Transfer files to this folder
    commit 1

---
+++ application/views/css_header.php
@@ -0,0 +1,23 @@
+<?php echo doctype("html5"); ?>
+<html>
+
+	<head>
+	    <meta charset="utf-8">
+	    <title>Flat UI Free 101 Template</title>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/vendor/bootstrap.min.css">
+	    <!-- Loading Flat UI -->
+
+
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>asset/css/flat-ui.css">
+
+
+	    <link rel="shortcut icon" href="<?php echo base_url(); ?>index.php/asset/img/favicon.ico">
+
+	    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+	    <!--[if lt IE 9]>
+	      <script src="js/vendor/html5shiv.js"></script>
+	      <script src="js/vendor/respond.min.js"></script>
+	    <![endif]-->
+	  </head>
---
+++ application/views/login_page.php
@@ -0,0 +1,35 @@
+
+	<body class="body">
+		<div class="container">
+      <div class="login">
+        <div class="login-page">
+        <div class="login-screen">
+
+          <div class="login-form">
+            <div class="form-group">
+              <input type="text" class="form-control login-field" value="" placeholder="Enter your name" id="login-name" />
+              <label class="login-field-icon fui-user" for="login-name"></label>
+            </div>
+
+            <div class="form-group">
+              <input type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" />
+              <label class="login-field-icon fui-lock" for="login-pass"></label>
+            </div>
+
+            <a class="btn btn-primary btn-lg btn-block" href="#">Log in</a>
+            <a class="login-link" href="#">Lost your password?</a>
+          </div>
+        </div>
+      </div>
+      </div>
+  </div>
+
+		
+
+	</body>
+    <script src="<?php echo base_url(); ?>asset/js/vendor/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?php echo base_url(); ?>asset/js/vendor/video.js"></script>
+    <script src="<?php echo base_url(); ?>asset/js/flat-ui.min.js"></script>
+       <script src="<?php echo base_url(); ?>asset/js/flat-ui.js"></script>
+</html>
